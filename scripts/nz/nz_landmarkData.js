98ac883ee1e6ad64ce04702ca8d66aa1db12d9e6
ste163 <samtedwards@pm.me>
2020-08-24 16:32:42 -0400

    added landmark JS

---
+++ scripts/nz/nz_landmarkData.js
@@ -0,0 +1,15 @@
+const landmarksArray = [
+    {
+        landmarkName: "Landmark 1"
+    },
+    {
+        landmarkName: "Landmark 2"
+    },
+    {
+        landmarkName: "Landmark 3"
+    }
+]
+
+export const landmarksArrayCopy = () => {
+    return landmarksArray.slice();
+}
--- scripts/nz/nz_main.js
+++ scripts/nz/nz_main.js
@@ -1,3 +1,5 @@
 import { addCity } from "./nz_cityAdd.js"
+import { addLandmark } from "./nz_landmarkAdd.js";
 
 addCity();
+addLandmark();
