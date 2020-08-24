4f1f0956479bae1eb5f16c44a28bf0ae518a4d11
ste163 <samtedwards@pm.me>
2020-08-24 15:55:25 -0400

    removed debugger

--- scripts/nz/nz_cityAdd.js
+++ scripts/nz/nz_cityAdd.js
@@ -2,12 +2,11 @@ import { cityArrayCopy } from "./nz_cityData.js";
 import { cityHTML } from "./nz_cityTemplateHTML.js";
 
 export const addCity = () => {
-
     const contentElement = document.querySelector(".citiesJs");
     const cityArray =  cityArrayCopy();
 
     let cityHTMLRepresentation = "";
-    debugger;
+    
     for (const eachCity of cityArray) {
         cityHTMLRepresentation  += cityHTML(eachCity);
     }
