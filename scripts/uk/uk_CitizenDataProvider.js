06ecf90b26fc99ca8813b72f8764ef7f41ea7e56
Tristanwyatt <tristan7306@gmail.com>
2020-08-24 19:52:44 -0500

    finshedJS

---
+++ scripts/uk/uk_CitizenDataProvider.js
@@ -0,0 +1,15 @@
+const cities = [
+    {
+        citizenName: 'Citizen 1'
+    },
+    {
+        citizenName: 'Citizen 2'
+    },
+    {
+        citizenName: 'Citizen 3'
+    }
+]
+
+export const citizenArray = () => {
+    return cities.slice();
+};
---
+++ scripts/uk/uk_LandmarkDataProvider.js
@@ -0,0 +1,15 @@
+const landmark = [
+    {
+        landmarkName: 'Landmark 1'
+    },
+    {
+        landmarkName: 'Landmark 2'
+    },
+    {
+        landmarkName: 'Landmark 3'
+    }
+]
+
+export const landmarkArray = () => {
+    return landmark.slice();
+};
